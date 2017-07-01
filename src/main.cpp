90fdffbd31544052f8adc55f4510133fa41cffb5
gijs.sijpesteijn <gijs@jpoint.nl>
2017-07-01 14:57:05 +0200

    Meer

--- src/main.cpp
+++ src/main.cpp
@@ -10,7 +10,8 @@
 
 int main( const int, const char** )
 {
-//	openlog ("Carmageddon", LOG_PID | LOG_CONS | LOG_NDELAY | LOG_NOWAIT, LOG_LOCAL0);
+	openlog ("Carmageddon", LOG_PID | LOG_CONS | LOG_NDELAY | LOG_NOWAIT, LOG_LOCAL0);
+	setlogmask(LOG_UPTO(LOG_ERR));
 	syslog (LOG_INFO, "%s", "Starting Carmageddon");
 
 	Car car;
