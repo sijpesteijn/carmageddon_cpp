50c4196fad38560d2d42d20eba29ea1c146add7f
gijs.sijpesteijn <gijs@jpoint.nl>
2017-08-13 13:42:10 +0200

    meer

--- html/src/app/manual/manual.routes.ts
+++ html/src/app/manual/manual.routes.ts
@@ -2,6 +2,7 @@ import { DetailComponent } from './detail.component';
 import { NoLifelineError } from './no_lifeline/no_lifeline.component';
 import { Routes } from '@angular/router';
 import { ObserverTabsComponent } from './observer-tabs.component';
+import { ManualComponent } from './manual.component';
 
 export const manual_routes: Routes = [
     { path: '', component: ManualComponent }
